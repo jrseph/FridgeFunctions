97ba65fd27038ceb6319f09c6bc917e55eadbe95
jrseph <91790254+jrseph@users.noreply.github.com>
2025-04-17 17:01:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FridgeFunctions",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'qcodes',
+        'numpy',
+        'time',
+        'logging',
+        'abc'
+    ],
+    description="A package for automating certain low-temperature measurements in CCMP, QMUL.",
+    author="Owen",
+    author_email="joseph.r.owen@qmul.ac"uk,
+    url="https://https://github.com/jrseph/FridgeFunctions",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)

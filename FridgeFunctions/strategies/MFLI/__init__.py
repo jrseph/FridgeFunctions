8e525bb9741c35afecd2267c19190dbd72c241ec
jrseph <91790254+jrseph@users.noreply.github.com>
2025-04-17 16:09:32 +0100

    Update __init__.py

--- FridgeFunctions/strategies/MFLI/__init__.py
+++ FridgeFunctions/strategies/MFLI/__init__.py
@@ -1,4 +1,5 @@
 from .auxout_dc_apply import AuxOutDCApply
 from .sigout_dc_apply import SigOutDCApply
+from .auxin_avg_measure import AuxInAvgMeasure
 
-__all__ = ["AuxOutDCApply", "SigOutDCApply"]
+__all__ = ["AuxOutDCApply", "SigOutDCApply", "AuxInAvgMeasure"]
